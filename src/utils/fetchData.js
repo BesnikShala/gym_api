af796656c958917a0b19495b5cdba539a39e2f34
BesnikShala <77018193+BesnikShala@users.noreply.github.com>
2023-02-28 17:10:02 +0000

    Add fetchdata api for youtube videos

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,8 +1,16 @@
 export const exerciseOptions = {
     method: 'GET',
     headers: {
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
       'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    },
+  };
+
+  export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
     }
   };
 
@@ -12,4 +20,4 @@ export const fetchData = async (url, options) => {
     const data = await response.json();
 
     return data;
-}
+};
